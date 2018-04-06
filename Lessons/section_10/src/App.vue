03eb500a35ec3f16536bc3c0f275632a3a09bc39
Juan HUERTA <HuertaJ@internal.ptc-prod.net>
2018-04-06 16:24:27 +0200

    Section 10 - My proposed solution

---
+++ Lessons/section_10/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+    	<input type="text" v-model='new_quote' >
+    	<button @click="addNewQuote(new_quote)">Add Quote</button>
+    	<div v-for='(quote_text,ind) in quotes'>
+    		<quote :delete_fnc='removeQuote' :quote_id='ind'>{{quote_text}}</quote>
+    	</div>
+    </div>
+</template>
+
+<script>
+	import Quote from './quote.vue';
+    export default {
+    	data: function(){
+    		return {
+    			quotes: ['one','two','three','four','five','six'],
+    			new_quote: '',
+    		}
+    	},
+  components: {
+    'quote': Quote,
+  },
+  methods:{
+  	addNewQuote(quote){
+  		console.log(quote);
+  		this.quotes.push(quote);
+  		this.new_quote = '';
+  	},
+  	removeQuote(ind)
+  	{
+  		console.log(ind);
+  		console.log(this.quotes);
+  		 this.quotes.splice(ind, 1);
+  	}
+  }
+    }
+</script>
+
+<style>
+</style>
---
+++ Lessons/section_10/src/quote.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <slot></slot>
+    <a @click='deleteQuote(quote_id)'>X</a>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: function() {
+    return {
+    
+    }
+  },
+  props:
+  {
+      quote_id:0,
+          delete_fnc: '',
+    },
+  methods : {
+    deleteQuote(ind)
+    {
+      this.delete_fnc(ind);
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  margin: 2px;
+  background-color: cyan;
+  border: blue 2px solid;
+}
+
+</style>
